a15985bedd24bde176ae8df7862f2fabeffdbeee
neemanjabu <nemanjabu@gmail.com>
2022-06-13 13:43:18 +0200

    Alpha3 updates (#2)
    * added countryToAlpha3
    * GB.json - added England to GB names
    * seed - added 2-3 conversion file
    updated countries

--- src/country-to-code.ts
+++ src/country-to-code.ts
@@ -8,6 +8,7 @@ const rootDir = __dirname.replace(/(src|lib)([\\\/]+[a-z]+)?$/, "");
 const dataDir = path.join(rootDir, "data");
 const alpha2s = JSON.parse(fs.readFileSync(path.join(dataDir, "iso-alpha-2.json")).toString());
 const alpha3s = JSON.parse(fs.readFileSync(path.join(dataDir, "iso-alpha-3.json")).toString());
+const alpha23s = JSON.parse(fs.readFileSync(path.join(dataDir, "iso-alpha-2-to-3.json")).toString());
 const names = JSON.parse(fs.readFileSync(path.join(dataDir, "names.json")).toString());
 
 /**
@@ -40,6 +41,21 @@ export const countryToAlpha2 = (str: any) : string|null  => {
   // Exact match
   if (names[country]) {
     return names[country];
+  }
+
+  return null;
+}
+
+/**
+ * Convert almost anything to an ISO 3166-1 alpha-3 code.
+ * @param str String to convert. Note: any is used so we can test non-string values returning null for CommonJS version.
+ */
+ export const countryToAlpha3 = (str: any) : string|null  => {
+   
+  const alpha2 = countryToAlpha2(str);
+
+  if (alpha2) {
+    return alpha23s[alpha2];
   }
 
   return null;
